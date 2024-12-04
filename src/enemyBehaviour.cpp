3a8d955b52d14d7060129e98db188f0b3e85c359
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-04 16:20:39 -0500

    Update enemyBehaviour.cpp

--- src/enemyBehaviour.cpp
+++ src/enemyBehaviour.cpp
@@ -9,5 +9,36 @@ void EnemyBehaviour::loadEnemies(string enemy1, string enemy2, string enemy3)
 
 void EnemyBehaviour::enemyActions()
 {
+    int num = randNum();
 
+    if (!isDead)
+    {
+        if (num == 1)
+        {
+            enemies[0].draw(100, 100);
+        }
+        if (num == 2)
+        {
+            enemies[1].draw(100, 100);
+        }
+        if (num == 3)
+        {
+            enemies[2].draw(100, 100);
+        }
+    }
+
+    //if (enemy is killed)
+    //{
+    //      isDead == true;
+    //      score++;
+    //    
+    //}
+}
+
+int EnemyBehaviour::randNum()
+{
+    int num;
+    srand(time(0)); //makes sure that the number is different everytime
+    num = (rand() % (3 - 1)) + 1;
+    return num;
 }
