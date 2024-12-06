6d0912a4abcf14df969466942d196420366ee42b
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-06 14:40:03 -0500

    load and draw ui

--- src/timer.cpp
+++ src/timer.cpp
@@ -10,7 +10,20 @@ void Timer::setText(string textPath, float size)
     m_timerText.load(textPath, size);
 }
 
-void Timer::drawText()
+void Timer::setTimerUI(string textPath)
 {
-    m_timerText.drawString("time: " + ofToString(m_time), 9 * CVC::APP_WINDOW_WIDTH / 10, 80.0f);
+    m_timerUI.load(textPath);
+}
+
+void Timer::drawText()
+{   
+    ofPushMatrix();
+    {
+        ofScale(0.8f, 0.8f);
+        m_timerUI.draw(1000.0f + CVC::APP_WINDOW_WIDTH / 2.0f, 0.0f);
+
+    }ofPopMatrix();
+   
+    ofSetColor(0);
+    m_timerText.drawString(ofToString(m_time), 950.0f + CVC::APP_WINDOW_WIDTH / 2.0f, 100.0f);
 }
