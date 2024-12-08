f2f713ba984b8aaebe6a9afb1bc20c923da1056a
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-08 18:13:53 -0500

    Update enemyBehaviour.h

--- src/enemyBehaviour.h
+++ src/enemyBehaviour.h
@@ -6,14 +6,19 @@ class EnemyBehaviour
 {
     private:
         ofImage enemies[3];
-        bool isDead;
-        int m_score;
         ofImage m_scoreUI;
+        int m_num;
+        ofTrueTypeFont m_scoreText;
 
     public:
+        bool isDead;
+        int m_score;
+        void setup();
         void loadEnemies(string enemy1, string enemy2, string enemey3);
+        void loadScoreText(string textPath, int size);
         void loadScoreUI(string textPath);
+        void drawScore();
         void enemyActions();
         void drawScoreUI();
-        int randNum();
+        void generateNew();
 };
