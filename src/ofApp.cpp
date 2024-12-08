a5c78365b786b1e078260116ae2fc4067a8d2841
sumaiyaibrahim04 <125154848+sumaiyaibrahim04@users.noreply.github.com>
2024-12-08 15:40:01 -0500

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,7 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
     //set up app
     ofSetWindowShape(CVC::APP_WINDOW_WIDTH, CVC::APP_WINDOW_HEIGHT);
     ofSetFrameRate(CVC::APP_DESIRED_FRAMERATE);
@@ -11,8 +11,8 @@ void ofApp::setup(){
     m_videoGrabber.setDeviceID(0);      //assuming first and only camera attached
     m_videoGrabber.setDesiredFrameRate(CVC::CAM_DESIRED_FRAMERATE);
     m_videoGrabber.initGrabber(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
-    m_camResolutionConflict = ( ((int)m_videoGrabber.getWidth() != CVC::VIDEO_WIDTH) ||
-                                ((int)m_videoGrabber.getHeight() != CVC::VIDEO_HEIGHT));
+    m_camResolutionConflict = (((int)m_videoGrabber.getWidth() != CVC::VIDEO_WIDTH) ||
+        ((int)m_videoGrabber.getHeight() != CVC::VIDEO_HEIGHT));
 
     //set up video
     m_videoPlayer.load(CVC::VIDEO_PATH_COLOR);
@@ -20,18 +20,18 @@ void ofApp::setup(){
     m_videoPlayer.play();
 
     //want to check to see if video being loaded in matches the video size consts we wish to process at
-    m_videoResolutionConflict = (   ((int)m_videoPlayer.getWidth() != CVC::VIDEO_WIDTH) || 
-                                    ((int)m_videoPlayer.getHeight() != CVC::VIDEO_HEIGHT) );
+    m_videoResolutionConflict = (((int)m_videoPlayer.getWidth() != CVC::VIDEO_WIDTH) ||
+        ((int)m_videoPlayer.getHeight() != CVC::VIDEO_HEIGHT));
 
     //allocate CV images
     m_colorImage.allocate(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
     m_grayscaleDiffImage.allocate(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
 
     //init vars
-    m_threshold             = 128;
-    m_numContoursConsidered = 5;
-    m_minArea               = 10.0f;
-    m_maxArea               = (float)(CVC::VIDEO_WIDTH * CVC::VIDEO_HEIGHT);
+    m_threshold = 100;
+    m_numContoursConsidered = 1;
+    m_minArea = 10.0f;
+    m_maxArea = (float)(CVC::VIDEO_WIDTH * CVC::VIDEO_HEIGHT);
     m_isSwinging = false;
     m_previousContourArea = 0.0f; //previous contour area
     m_farThreshold = 180.0f; //"far" area
@@ -41,21 +41,22 @@ void ofApp::setup(){
     m_magicSound.load("magic-spell.mp3");
 
     //wave effect
-    waveInterval = 500;  // Time between waves in milliseconds
+    waveInterval = 500;  //time between waves in milliseconds
     lastWaveTime = 0;
     stopWaves = false;
     waveCount = 0;
     maxWaves = 10;
 
+
     //timer
     m_timer.setText("generalText.ttf", 50.0f);
     m_timer.setTimerUI("timer.png");
 
-    //enemies
-    m_enemies.loadEnemies("green-enemy.png", "blue-enemy.png", "pink-enemy.png");
-
     //debug view
     m_showDebugView = false;
+
+    //enemies
+    m_enemies.loadEnemies("green-enemy.png", "blue-enemy.png", "pink-enemy.png");
 
     //magic wand cursor 
     cursor.load("magicWand.png");
@@ -72,65 +73,60 @@ void ofApp::setup(){
     m_appModes.push_back("APP_CAM");
 }
 
+
 //--------------------------------------------------------------
-void ofApp::update(){
-    
+void ofApp::update() {
     switch (m_appMode) {
-        case CVC::APP_MODE::APP_VIDEO: {
-            m_videoPlayer.update();
+    case CVC::APP_MODE::APP_VIDEO: {
+        m_videoPlayer.update();
 
-            //only process if new frame
-            if (m_videoPlayer.isFrameNew() == true) {
-                m_colorImage.setFromPixels(m_videoPlayer.getPixels());
+        if (m_videoPlayer.isFrameNew()) {
+            m_colorImage.setFromPixels(m_videoPlayer.getPixels());
 
-                //resize, if res conflict
-                if (m_videoResolutionConflict == true) {
-                    m_colorImage.resize(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
-                }
+            if (m_videoResolutionConflict) {
+                m_colorImage.resize(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
 
-                processColor(m_colorImage);
             }
+
+           // processColor(m_colorImage);
+            AutoTrackColor(m_colorImage, pinkRange, greenRange, blueRange);
         }
-            break;
-        case CVC::APP_MODE::APP_CAM: {
-            if (m_camPaused == false) {
-                m_videoGrabber.update();
+    } break;
 
-                if (m_videoGrabber.isFrameNew() == true) {
-                    m_colorImage.setFromPixels(m_videoGrabber.getPixels());
-                }
+    case CVC::APP_MODE::APP_CAM: {
+        if (!m_camPaused) {
+            m_videoGrabber.update();
 
-                //resize, if res conflict
-                if (m_camResolutionConflict == true) {
-                    m_colorImage.resize(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
-                }
-
-                processColor(m_colorImage);
+            if (m_videoGrabber.isFrameNew()) {
+                m_colorImage.setFromPixels(m_videoGrabber.getPixels());
             }
-            
+
+            if (m_camResolutionConflict) {
+                m_colorImage.resize(CVC::VIDEO_WIDTH, CVC::VIDEO_HEIGHT);
+            }
+
+            //processColor(m_colorImage);
+            AutoTrackColor(m_colorImage, pinkRange, greenRange, blueRange);
         }
-            break;
+    } break;
     }
 
-    // Swing detection logic
+    //swing detection logic
     if (m_contourFinder.nBlobs > 0) {
-        float currentContourArea = m_contourFinder.blobs[0].area; // Use the first blob for simplicity
+        float currentContourArea = m_contourFinder.blobs[0].area;
 
         if (!m_isSwinging && currentContourArea < m_farThreshold) {
-            m_isSwinging = true; // Start the swing
+            m_isSwinging = true; //swing start
         }
         else if (m_isSwinging && currentContourArea > m_closeThreshold) {
-            m_isSwinging = false; // End the swing
-            // Trigger the swing event here
-            //ofDrawBitmapString("SWING DETECTED", ofGetWindowWidth()/2, ofGetWindowHeight() / 2);
-          //  ofLogNotice() << "Swing detected! Playing sound.";
+            m_isSwinging = false; //swing end
             m_magicSound.play();
 
             // Create a new wave at the blob's position
-            ofVec2f wavePosition = ofVec2f(m_contourFinder.blobs[0].centroid.x,
-                m_contourFinder.blobs[0].centroid.y);
+            //ofVec2f wavePosition = ofVec2f(m_contourFinder.blobs[0].centroid.x,
+              //  m_contourFinder.blobs[0].centroid.y);
             Wave newWave;
-            newWave.position = wavePosition;
+            newWave.position.set(CVC::APP_WINDOW_WIDTH / 2, CVC::APP_WINDOW_HEIGHT / 2);
             newWave.radius = 0;
             newWave.opacity = 255;
             newWave.maxRadius = 300;
@@ -141,58 +137,65 @@ void ofApp::update(){
         m_previousContourArea = currentContourArea; // Update the previous area
     }
 
-   
 
-     // Update waves
+
+    // Update waves
     for (int i = waves.size() - 1; i >= 0; --i) {
-        waves[i].radius += 5;   // Expand radius
-        waves[i].opacity -= 5;  // Fade opacity
+        waves[i].radius += 5;   //expand radius
+        waves[i].opacity -= 5;  //fade opacity
         if (waves[i].opacity < 0) waves[i].opacity = 0;
 
-        // Remove "dead" waves
+        //remove "dead" waves
         if (waves[i].radius > waves[i].maxRadius || waves[i].opacity <= 0) {
             waves.erase(waves.begin() + i);
         }
     }
 
-    // update the timer
+    //update timer
     m_timer.update();
 
-    //CODE FOR CURSOR WAND STARS
-    //generates the random positions
+    //cursor wand stars
     float x = ofGetMouseX() + ofRandom(-50, 50);
     float y = ofGetMouseY() + ofRandom(-50, 50);
-    //generates the rndm sizes
     float size = ofRandom(10, 50);
 
-    //puts the new position and sie of the stars inside the vectors
     starPositions.push_back(ofVec2f(x, y));
     starSizes.push_back(size);
 
-    //the 10 determines how many stars you want at a time for more particules do 50 
     if (starPositions.size() > 10) {
-        //removes the position and size of oldest star added in the vecor
-        //deleting it basically
         starPositions.erase(starPositions.begin());
         starSizes.erase(starSizes.begin());
     }
+    ofLogNotice() << "r: " << static_cast<int>(std::round(m_trackedColor[2] * 255));
+    ofLogNotice() << "g: " << static_cast<int>(std::round(m_trackedColor[2] * 255));
+    ofLogNotice() << "b: " << static_cast<int>(std::round(m_trackedColor[2] * 255));
+
 }
 
+
+
+
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+
+
 
     ofPushMatrix();
-    {
-        ofSetColor(255);
-        // ofTransform();
-        ofScale(2, 2);
-        m_skyBg.draw(0, 0);
-        
-    }ofPopMatrix();
+
+    ofSetColor(255);
+    ofScale(2, 2);
+    m_skyBg.draw(0, 0);
+
+    ofPopMatrix();
+
+
+    ofPushMatrix();
 
     // draw timer
     m_timer.drawText();
-   
+
+    ofPopMatrix();
+
 
     if (m_showDebugView) {
 
@@ -200,7 +203,6 @@ void ofApp::draw(){
         ofSetColor(255, 255, 255);
         m_grayscaleDiffImage.draw(CVC::VIDEO_BORDER_SIZE, CVC::VIDEO_BORDER_SIZE);
         m_colorImage.draw(CVC::VIDEO_WIDTH + CVC::VIDEO_BORDER_SIZE * 2.0f, CVC::VIDEO_BORDER_SIZE);
-        //m_magicSparkles.draw(CVC::VIDEO_WIDTH + CVC::VIDEO_BORDER_SIZE * 2.0f, CVC::VIDEO_BORDER_SIZE);
 
 
         //draw contours
@@ -261,7 +263,7 @@ void ofApp::draw(){
         //sets the max desired size
     star.resize(50, 50);
 
-    //draws the starst
+    //draws the stars
     for (int i = 0; i < starPositions.size(); i++) {
 
         float size = starSizes[i];
@@ -278,37 +280,25 @@ void ofApp::draw(){
     cursor.draw(ofGetMouseX() - 50, ofGetMouseY() - 60);
     cursor.resize(100, 100);
 
-    // Draw waves
+    //draw the waves
+
     ofPushMatrix();
 
     for (const Wave& wave : waves) {
 
-        //ofSetColor(255, 255, 255, wave.opacity);  // White with fading opacity
-        //ofSetColor(0, 255, 0, wave.opacity); //green 
-       // ofSetColor(0, 0, 255, wave.opacity); //blue
-        ofSetColor(255, 105, 180, wave.opacity); //pink
-        ofNoFill();
-        ofSetLineWidth(5);
-
-        // Draw multiple rings around the wave's position
-        int numRings = 5; // Number of rings
-        float spacing = 50; // Space between rings
-        for (int i = 0; i < numRings; ++i) {
-            float ringRadius = wave.radius + (i * spacing);
-            if (ringRadius <= wave.maxRadius) {
-                ofDrawCircle(wave.position, ringRadius);
-            }
-        } 
+        drawWaves(wave);
     }
+
+                
+
+
 
     ofPopMatrix();
 
-
-    
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
     //each key on a keyboard ha sa keycode associated with it
     //32 = spacebar
     if (key == 32) {
@@ -317,11 +307,11 @@ void ofApp::keyPressed(int key){
             //set the opposite of what pause state it alrready is
             m_videoPlayer.setPaused(!m_videoPlayer.isPaused());
         }
-            break;
+                                     break;
         case CVC::APP_MODE::APP_CAM: {
             m_camPaused = !m_camPaused;
         }
-           break;
+                                   break;
         }
     }
 
@@ -337,15 +327,15 @@ void ofApp::keyPressed(int key){
 
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
     //let's do this in a bit 
     if (button == OF_MOUSE_BUTTON_RIGHT) {
         //select the colour from the image
         //we CAN't SELECT color from outside the image (may get an indes out of array error)
-        ofRectangle videoRect = ofRectangle( CVC::VIDEO_WIDTH + CVC::VIDEO_BORDER_SIZE * 2,
-                                             CVC::VIDEO_BORDER_SIZE,
-                                             CVC::VIDEO_WIDTH,
-                                             CVC::VIDEO_HEIGHT );
+        ofRectangle videoRect = ofRectangle(CVC::VIDEO_WIDTH + CVC::VIDEO_BORDER_SIZE * 2,
+            CVC::VIDEO_BORDER_SIZE,
+            CVC::VIDEO_WIDTH,
+            CVC::VIDEO_HEIGHT);
 
         //step 1: clamping screenspace to image space
         int convertX = ofClamp(x, videoRect.getMinX(), videoRect.getMaxX());
@@ -363,38 +353,110 @@ void ofApp::mousePressed(int x, int y, int button){
     }
 }
 
-void ofApp::processColor(ofxCvColorImage& image) {
-    const int numChannelsPerPixel   = image.getPixels().getNumChannels(); //shouild be 3 for red, green, blue channels
-    const int numChannels           = CVC::VIDEO_WIDTH * CVC::VIDEO_HEIGHT * numChannelsPerPixel;
-    float pixel[3]                  = { 0.0f, 0.0f, 0.0f };
+void ofApp::AutoTrackColor(ofxCvColorImage& image, ColorRange cr1, ColorRange cr2, ColorRange cr3) {
+    const int numChannelsPerPixel = image.getPixels().getNumChannels(); // should be 3 for red, green, blue channels
+    const int numChannels = CVC::VIDEO_WIDTH * CVC::VIDEO_HEIGHT * numChannelsPerPixel;
+    float pixel[3] = { 0.0f, 0.0f, 0.0f };
 
-    //loop through color image pixels
-    //check if pixel color matches our tracked color
-    //if true, set our diff image pixel color to white
-    //if false, set our diff image pixel color to black
-    for (int i = 0; i < numChannels; i+=numChannelsPerPixel) {
-        pixel[0] = image.getPixels()[i + 0];        //red
-        pixel[1] = image.getPixels()[i + 1];        //green
-        pixel[2] = image.getPixels()[i + 2];        //blue
+    ofPixels& pixels = image.getPixels();
+    ofImage binaryMask;
+    binaryMask.allocate(image.getWidth(), image.getHeight(), OF_IMAGE_GRAYSCALE);
 
-        //check if tracked color matches pixel color
-        //m_trackedColor is a float color [0.0, 1.0] so we need to convert back to a byte color [0,255]
-        if (    (abs(pixel[0] - m_trackedColor[0] * 255.0f) < m_threshold) &&
-                (abs(pixel[1] - m_trackedColor[1] * 255.0f) < m_threshold) &&
-                (abs(pixel[2] - m_trackedColor[2] * 255.0f) < m_threshold)) {
-            
-            //COLOR THE SAME _ YAAYYYYYYY!!
+    // loop through the image pixels
+    for (int i = 0; i < numChannels; i += numChannelsPerPixel) {
+        pixel[0] = pixels[i + 0];        // red
+        pixel[1] = pixels[i + 1];        // green
+        pixel[2] = pixels[i + 2];        // blue
+
+        //check if the pixel matches any of the defined color ranges
+        bool matchesPink = pixel[0] >= cr1.minR && pixel[0] <= cr1.maxR &&
+            pixel[1] >= cr1.minG && pixel[1] <= cr1.maxG &&
+            pixel[2] >= cr1.minB && pixel[2] <= cr1.maxB;
+
+        bool matchesGreen = pixel[0] >= cr2.minR && pixel[0] <= cr2.maxR &&
+            pixel[1] >= cr2.minG && pixel[1] <= cr2.maxG &&
+            pixel[2] >= cr2.minB && pixel[2] <= cr2.maxB;
+
+        bool matchesBlue = pixel[0] >= cr3.minR && pixel[0] <= cr3.maxR &&
+            pixel[1] >= cr3.minG && pixel[1] <= cr3.maxG &&
+            pixel[2] >= cr3.minB && pixel[2] <= cr3.maxB;
+
+        //if the pixel matches any of the color ranges, set corresponding grayscale value to white
+        if (matchesPink || matchesGreen || matchesBlue) {
             m_grayscaleDiffImage.getPixels()[i / numChannelsPerPixel] = 255;
+            //if (matchesPink) {
+            //    m_trackedColor[0] = pixel[0] / 255.0f;  // red
+            //    m_trackedColor[1] = pixel[1] / 255.0f;  // green
+            //    m_trackedColor[2] = pixel[2] / 255.0f;  // blue
+            //}
+            //else if (matchesGreen) {
+            //    m_trackedColor[0] = pixel[0] / 255.0f;  // red
+            //    m_trackedColor[1] = pixel[1] / 255.0f;  // green
+            //    m_trackedColor[2] = pixel[2] / 255.0f;  // blue
+            //}
+            //else if (matchesBlue) {
+            //    m_trackedColor[0] = pixel[0] / 255.0f;  // red
+            //    m_trackedColor[1] = pixel[1] / 255.0f;  // green
+            //    m_trackedColor[2] = pixel[2] / 255.0f;  // blue
+            //}
         }
         else {
-            //NOT THE SAME - BOOO
             m_grayscaleDiffImage.getPixels()[i / numChannelsPerPixel] = 0;
         }
     }
 
-    //update cv image
-    m_grayscaleDiffImage.flagImageChanged();        //tells the compy that the image has changed on the CPU RAM and should be moved to GPU RAM
-    
+    // update the grayscale image
+    m_grayscaleDiffImage.flagImageChanged(); // marks the image as changed for further processing
+
     //find contours/blobs
     m_contourFinder.findContours(m_grayscaleDiffImage, m_minArea, m_maxArea, m_numContoursConsidered, false, true);
 }
+
+
+//--------------------------------------------------------------
+void ofApp::drawWaves(const Wave& wave) {
+    
+    
+    ofNoFill();
+    ofSetLineWidth(5);
+    
+    int red = static_cast<int>(std::round(m_trackedColor[0] * 255));
+    int green = static_cast<int>(std::round(m_trackedColor[1] * 255));
+    int blue = static_cast<int>(std::round(m_trackedColor[2] * 255));
+
+    //check ranges for pink, green, and blue
+    //if (red >= 187 && red <= 249 &&
+    //    green >= 45 && green <= 199 &&
+    //    blue >= 114 && blue <= 244) {
+    //    ofSetColor(255, 105, 180, wave.opacity); // pink
+    //}
+    //else if (red >= 0 && red <= 92 &&
+    //    green >= 191 && green <= 231 &&
+    //    blue >= 88 && blue <= 149) {
+    //    ofSetColor(0, 255, 0, wave.opacity); // green
+    //}
+    //else if (red >= 37 && red <= 119 &&
+    //    green >= 54 && green <= 166 &&
+    //    blue >= 196 && blue <= 247) {
+    //    ofSetColor(0, 0, 255, wave.opacity); //blue
+    //}
+    //else {
+     ofSetColor(255, 255, 255, wave.opacity); //default to white
+    //}
+    
+
+    //draw the wave circle
+   // ofDrawCircle(wave.position, wave.radius);
+    //draw multiple rings around the wave's position
+    int numRings = 5; // number of rings
+    float spacing = 50; //space between rings
+    for (int i = 0; i < numRings; ++i) {
+        float ringRadius = wave.radius + (i * spacing);
+        if (ringRadius <= wave.maxRadius) {
+            ofDrawCircle(wave.position, ringRadius);
+        }
+    }
+}
+
+
+
